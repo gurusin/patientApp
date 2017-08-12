300f0528113c0c76ff4d9ca0c2a0a3f8dc7e610b
radika <radikaj@yahoo.com>
2017-08-12 08:04:02 +0530

    Added base classes and initial layout

---
+++ frontend/src/app/treatment/basicvisit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basicvisit',
+  templateUrl: './basicvisit.component.html',
+  styleUrls: ['./basicvisit.component.css']
+})
+export class BasicvisitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/src/app/treatment/patientnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patientnavbar',
+  templateUrl: './patientnavbar.component.html',
+  styleUrls: ['./patientnavbar.component.css']
+})
+export class PatientnavbarComponent {
+
+}
