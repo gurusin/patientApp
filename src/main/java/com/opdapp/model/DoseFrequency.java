b2aa781f8f9f81a39da28a4c4dcebb36fdbacf77
radika.jayaweera <radikaj@yahoo.com>
2017-08-13 16:33:08 +0530

    Returned searched drug

--- src/main/java/com/opdapp/model/DoseFrequency.java
+++ src/main/java/com/opdapp/model/DoseFrequency.java
@@ -14,4 +14,28 @@ public class DoseFrequency {
     private String timeUnit;
     private int noofDoses;
 
+
+    public int getDoseFrequencyId() {
+        return doseFrequencyId;
+    }
+
+    public void setDoseFrequencyId(int doseFrequencyId) {
+        this.doseFrequencyId = doseFrequencyId;
+    }
+
+    public String getTimeUnit() {
+        return timeUnit;
+    }
+
+    public void setTimeUnit(String timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    public int getNoofDoses() {
+        return noofDoses;
+    }
+
+    public void setNoofDoses(int noofDoses) {
+        this.noofDoses = noofDoses;
+    }
 }
