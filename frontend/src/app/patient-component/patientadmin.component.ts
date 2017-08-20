ec23306dbd0be9d8f996eb081525aaa973536550
radika.jayaweera <radikaj@yahoo.com>
2017-08-20 10:15:01 +0530

    Moved selectd patient for Edit

--- frontend/src/app/patient-component/patientadmin.component.ts
+++ frontend/src/app/patient-component/patientadmin.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PatientServiceService} from "../patient-service.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-patientadmin',
@@ -8,13 +9,15 @@ import {PatientServiceService} from "../patient-service.service";
 })
 export class PatientadminComponent implements OnInit {
 
-  selectedRow : Number;
-  setClickedRow : Function;
+  selectedRow: Number;
+  setClickedRow: Function;
 
-  patientList =[];
-  constructor(private patientService:PatientServiceService) {
-    this.setClickedRow = function(index){
+  patientList = [];
+
+  constructor(private patientService: PatientServiceService, private router: Router) {
+    this.setClickedRow = function (index) {
       this.selectedRow = index;
+      this.patientService.patientObject = this.patientList[index];
     }
 
   }
@@ -22,9 +25,22 @@ export class PatientadminComponent implements OnInit {
   ngOnInit() {
     this.patientService.loadPatients(
     ).subscribe(
-      data =>{this.patientList =data;
+      data => {
+        this.patientList = data;
       }
     );
+  }
+
+  onEditPatient() {
+    {
+      if (this.patientService.patientObject == null) {
+        alert(" Please select a patient ");
+      } else {
+        this.router.navigate(["patientsave"]);
+
+      }
+    }
+
   }
 
 }
