792333a682efe032eca9c9caaf1eff37ec99ab79
Sudarshana Gurusinghe <sgurusinghe@gmail.com>
2017-09-16 06:41:26 +0530

    Added validation to prevent prescription getting submitted with empty fields

--- frontend/src/app/treatment/basicvisit.component.ts
+++ frontend/src/app/treatment/basicvisit.component.ts
@@ -14,89 +14,76 @@ import {PrintpopComponent} from "./printpop.component";
 // import {Popup} from "ng2-opd-popup";
 
 @Component({
-  selector: 'app-basicvisit',
-  templateUrl: './basicvisit.component.html',
-  styleUrls: ['./basicvisit.component.css']
+    selector: 'app-basicvisit',
+    templateUrl: './basicvisit.component.html',
+    styleUrls: ['./basicvisit.component.css']
 })
 export class BasicvisitComponent implements OnInit {
 
-  patientVisit: Patientvisit;
-  @Input() patient: Patient;
+    patientVisit: Patientvisit;
+    @Input() patient: Patient;
 
-  // dialogRef: MdDialogRef<PrintpopComponent>;
+    // dialogRef: MdDialogRef<PrintpopComponent>;
 
-  constructor(private patientService: PatientServiceService,
-              private drugService: DrugServiceService, private router: Router){
-              // public mdDialog: MdDialog){//}, private popup:Popup) {
-    this.patientVisit = new Patientvisit();
-    this.patientVisit.diagnoseData = '';
-    this.patientVisit.prescribableDrug = []
-  }
+    constructor(private patientService: PatientServiceService,
+                private drugService: DrugServiceService, private router: Router) {
+        // public mdDialog: MdDialog){//}, private popup:Popup) {
+        this.patientVisit = new Patientvisit();
+        this.patientVisit.diagnoseData = '';
+        this.patientVisit.prescribableDrug = []
+    }
 
-    onCancel()
-    {
+    onCancel() {
         // Navigate to any page
     }
 
-  ngOnInit() {
-  }
-
-  onSave() {
-    var presc = new Prescription();
-    presc.medicalServices = this.patientVisit.medicalServices;
-    presc.prescriptionDetails = [];
-    presc.patientId = this.patientService.patientObject.patientId;
-    presc.diagnosis = this.patientVisit.diagnoseData;
-    presc.notes = this.patientVisit.note;
-    presc.symptoms = this.patientVisit.symptoms;
-
-    var details = [];
-    this.patientVisit.prescribableDrug.forEach((obj: PrescribableDrug) => {
-        var detail = new PrescriptionDetail();
-        detail.drugId = obj.drug.drugId;
-        detail.drug.drugId = obj.drug.drugId;
-        detail.amount = obj.doseAmount;
-        detail.duration = obj.doseDuration;
-        detail.strength = obj.strengths[obj.selectedStrengthIndex];
-        detail.frequency.doseFrequencyId = obj.selectedFrequency;
-        detail.intervalUnit = obj.selectedDuration;
-        detail.meal = obj.meal;
-        details.push(detail);
-      }
-    );
-    presc.prescriptionDetails = details;
-    this.drugService.savePrescription(presc).subscribe(
-        data =>{
-            this.drugService.setSavedPrescription(data);
-            this.router.navigate(['printPo']);
-        }
-    );
-
-    //TODO Remove this once pop up is done
-    //window.location.reload();
-
-    // this.popup.options = {
-    //   header: "Print Prescription ",
-    //   color: "#5cb85c", // red, blue....
-    //   widthProsentage: 40, // The with of the popou measured by browser width
-    //   animationDuration: 0, // in seconds, 0 = no animation
-    //   // showButtons: true, // You can hide this in case you want to use custom buttons
-    //   // confirmBtnContent: "OK", // The text on your confirm button
-    //   // cancleBtnContent: "Cancel", // the text on your cancel button
-    //   confirmBtnClass: "btn btn-default", // your class for styling the confirm button
-    //   cancleBtnClass: "btn btn-default", // you class for styling the cancel button
-    //   animation: "fadeInDown", // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown'
-    //
-    // };
-
-
-    // this.popup.show(this.popup.options);
-  }
-
-    showPopup()
-    {
-        // this.dialogRef = this.mdDialog.open(PrintpopComponent);
+    ngOnInit() {
     }
 
+    onSave() {
+        var presc = new Prescription();
+        presc.medicalServices = this.patientVisit.medicalServices;
+        presc.prescriptionDetails = [];
+        presc.patientId = this.patientService.patientObject.patientId;
+        presc.diagnosis = this.patientVisit.diagnoseData;
+        presc.notes = this.patientVisit.note;
+        presc.symptoms = this.patientVisit.symptoms;
+        var details = [];
+        var bOK = true;
+        this.patientVisit.prescribableDrug.forEach((obj: PrescribableDrug) => {
+                var detail = new PrescriptionDetail();
 
+                detail.drugId = obj.drug.drugId;
+                detail.drug.drugId = obj.drug.drugId;
+                detail.amount = obj.doseAmount;
+                detail.duration = obj.doseDuration;
+                if (obj.strengths == null)
+                {
+                    alert('Prescription has empty fields');
+                    bOK=false;
+                    return;
+                }
+                detail.strength = obj.strengths[obj.selectedStrengthIndex];
+                detail.frequency.doseFrequencyId = obj.selectedFrequency;
+                detail.intervalUnit = obj.selectedDuration;
+                detail.meal = obj.meal;
+                if (!detail.isValid()){
+                    alert("Empty rows found. Can't submit");
+                    bOK = false;
+                    return;
+                }
+                details.push(detail);
+            }
+        );
+        if (!bOK) {
+            return;
+        }
+        presc.prescriptionDetails = details;
+        this.drugService.savePrescription(presc).subscribe(
+            data => {
+                this.drugService.setSavedPrescription(data);
+                this.router.navigate(['printPo']);
+            }
+        );
+    }
 }
