afde6d586d9f57acf497d90d4221d1c475be47db
gurusin <sgurusinghe@gmail.com>
2017-10-01 16:25:56 +0530

    Cleared selected patient on logoff

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {LoginService} from "./services/login-service.service";
 import {Router} from "@angular/router";
+import {PatientServiceService} from "./services/patient-service.service";
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,15 @@ export class AppComponent {
   logged = false;
 
 
-  constructor(private loginService:LoginService, private router:Router) {
+  constructor(private loginService:LoginService, private router:Router,
+              private patientService:PatientServiceService) {
     this.logged = this.loginService.loggedInUser !==null;
   }
 
   logOff() {
     this.loginService.loggedInUser = null;
     this.loginService.loggedIn = false;
+    this.patientService.patientObject = null;
     this.router.navigate(['']);
 
   }
