b605257116eff6b3b8e5805e124cb85e0e41388b
gurusin <sgurusinghe@gmail.com>
2017-10-01 15:34:09 +0530

    Change in prescription to enter row by row and show price on the spot

--- frontend/src/app/prescription-component/prescribable-drug.ts
+++ frontend/src/app/prescription-component/prescribable-drug.ts
@@ -1,33 +1,40 @@
 import {Drug} from "../drugcomponent/drug";
 import {Strength} from "../drugcomponent/strength";
 import {Dosefrequency} from "../drugcomponent/dosefrequency";
+
 export class PrescribableDrug {
 
-  public drug:Drug;
-  strengths:Strength[];
-  doseFrequency:Dosefrequency[];
-  selectedStrengthIndex :0;
-  doseAmount:number;
-  doseDuration:number;
-  durationUnit:string[];
-  selectedStrength:Strength;
-  selectedFrequency:number;
-  selectedDuration:string;
-  meal:string;
-  drugList:any[];
-  rowId:number;
-  packages:any[];
-  availableQty:any;
-  unitPrice=0.00;
+  public drug: Drug;
+  doseFrequency: Dosefrequency[];
+  selectedStrengthIndex: 0;
+  doseAmount: number;
+  doseDuration: number;
+  durationUnit: string[];
+  selectedStrength: Strength;
+  selectedFrequency: number;
+  selectedDuration: string;
+  meal: string;
+  drugList: any[];
+  rowId: number;
+  packages: any[];
+  availableQty: any;
+  unitPrice = 0.00;
+  price = 0.00;
+  neededQty = 0;
+  frequency : Dosefrequency;
+  valid=false;
 
-  constructor()
-  {
-    this.drug=new Drug();
+  constructor() {
+    this.drug = new Drug();
     this.rowId = new Date().getMilliseconds();
     this.selectedStrength = new Strength();
+    this.valid = false;
   }
 
-    public findAvailableQty() {
-        this.availableQty = this.packages[this.selectedStrengthIndex].quantity;
-    }
+  public findAvailableQty() {
+    this.availableQty = this.packages[this.selectedStrengthIndex].quantity;
+  }
+
+
+
 }
--- frontend/src/app/prescription-component/prescription-component.component.ts
+++ frontend/src/app/prescription-component/prescription-component.component.ts
@@ -6,7 +6,7 @@ import {DrugSearchResult} from "app/prescription-component/drug-search-result";
 import {forEach} from "@angular/router/src/utils/collection";
 import {PrescribableDrug} from "./prescribable-drug";
 import 'rxjs/Rx';
-import {Http, RequestMethod, RequestOptions,Response} from "@angular/http";
+import {Http, RequestMethod, RequestOptions, Response} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import {Patient} from "../patient";
 import {PatientServiceService} from "../services/patient-service.service";
@@ -23,52 +23,72 @@ import {Strength} from "../drugcomponent/strength";
 export class PrescriptionComponentComponent implements OnInit {
 
   @Input() patientVisit: Patientvisit;
-  selectedId:any;
+  prescribableDrug: PrescribableDrug;
+  selectedId: any;
+  drugId = 0;
+  total = 0;
 
   constructor(private drugService: DrugServiceService,
-              private patientService:PatientServiceService,
-              private router:Router) {
+              private patientService: PatientServiceService,
+              private router: Router) {
   }
 
   ngOnInit() {
-    var obj = new PrescribableDrug();
-    this.patientVisit.prescribableDrug.push(obj);
+    this.prescribableDrug = new PrescribableDrug();
   }
 
-  doSearch(drugName:string,row:PrescribableDrug) {
+  doSearch(drugName: string) {
     this.drugService.searchDrug(drugName).subscribe(
       data => {
-        row.drugList = data;
+        this.prescribableDrug.drugList = data;
       }
     );
   }
 
-  addDrug()
-  {
-      this.patientVisit.prescribableDrug.forEach((obj: PrescribableDrug) => {
-        console.log(obj);
-      });
-    this.patientVisit.prescribableDrug.push(new PrescribableDrug());
+  addDrug() {
+    var x = new PrescribableDrug();
+    this.prescribableDrug.frequency = this.prescribableDrug.doseFrequency[this.prescribableDrug.selectedFrequency];
+    Object.assign(x,this.prescribableDrug);
+    this.patientVisit.prescribableDrug.unshift(x);
+    this.prescribableDrug = new PrescribableDrug();
+    this.calculateTotal();
   }
 
-  private searchPrescribable(row:PrescribableDrug,i:number) {
-    this.drugService.searchPrescribable(this.selectedId).subscribe(
+  private searchPrescribable() {
+    this.drugService.searchPrescribable(this.drugId).subscribe(
       data => {
-        this.patientVisit.prescribableDrug.splice(i,1,data);
-        this.patientVisit.prescribableDrug[i].selectedStrengthIndex =0;
-        this.selectStrength(0);
+        this.prescribableDrug = data;
+        this.prescribableDrug.selectedStrength = this.prescribableDrug.packages[0].strength;
+        this.prescribableDrug.selectedStrengthIndex =0;
+        this.selectStrength();
       });
   }
 
-    selectStrength(i)
-    {
-       var obj = this.patientVisit.prescribableDrug[i];
-       this.patientVisit.prescribableDrug[i].availableQty = obj.packages[obj.selectedStrengthIndex].quantity;
-      this.patientVisit.prescribableDrug[i].unitPrice =obj.packages[obj.selectedStrengthIndex].unitPrice;
-      this.patientVisit.prescribableDrug[i].meal='ORAL';
-    }
+  public checkValid()
+  {
+    this.prescribableDrug.valid =
+      this.prescribableDrug.drug &&
+      this.prescribableDrug.selectedStrengthIndex >-1 &&
+      this.prescribableDrug.selectedFrequency >-1 &&
+      this.prescribableDrug.doseAmount > 0 && this.prescribableDrug.doseDuration >0;
+      this.prescribableDrug.selectedDuration && this.prescribableDrug.meal;
 
-  setDrugId(pres:PrescribableDrug) {
+      if (this.prescribableDrug.valid)
+      {
+        this.calculatePrice();
+      }
+
+
+  }
+
+
+  selectStrength() {
+    this.prescribableDrug.availableQty = this.prescribableDrug.packages[this.prescribableDrug.selectedStrengthIndex].quantity;
+    this.prescribableDrug.unitPrice = this.prescribableDrug.packages[this.prescribableDrug.selectedStrengthIndex].unitPrice;
+    this.prescribableDrug.meal = 'ORAL';
+  }
+
+  setDrugId(pres: PrescribableDrug) {
     pres.drugList.forEach((obj: DrugSearchResult) => {
         if (obj.brandName === pres.drug.brandName) {
           this.selectedId = obj.drugId;
@@ -78,55 +98,107 @@ export class PrescriptionComponentComponent implements OnInit {
     );
   }
 
-  removeDrug(i:number)
-  {
-    if (i>0)
-    {
-      this.patientVisit.prescribableDrug.
-      splice(i,1);
-    }
-
+  removeDrug(i: number) {
+      this.patientVisit.prescribableDrug.splice(i, 1);
+      this.calculateTotal();
   }
 
-  searchDrug(event: any, row:PrescribableDrug,i:number) {
+  searchDrug(event: any, searchText: string) {
+    if (event.keyCode == 13) {
+      this.searchPrescribable();
+    } else if (searchText.length == 2) {
+      // Search for drug
+      this.doSearch(searchText);
+    } else if (searchText.length == 1) {
+      //clear search
+      this.prescribableDrug.drugList = [];
+    }
+  }
+
+  searchDrug_dep(event: any, row: PrescribableDrug, i: number) {
     var drugName = row.drug.brandName;
     if (event.keyCode == 13) {
       // Search for prescibable
       this.setDrugId(row);
-      this.searchPrescribable(row,i);
+      this.searchPrescribable();
     } else if (drugName.length == 2) {
       // Search for drug
-      this.doSearch(drugName,row);
+      //this.doSearch(drugName,row);
     } else if (drugName.length == 1) {
       //clear search
       row.drugList = [];
     }
   }
+
+  calculatePrice() {
+    this.calculateQty(this.prescribableDrug);
+    this.prescribableDrug.price = this.prescribableDrug.unitPrice * this.prescribableDrug.neededQty;
+  }
+
+  calculateTotal()
+  {
+     this.total =0;
+     this.patientVisit.prescribableDrug.forEach(
+       obj =>{
+         this.total += obj.price;
+       }
+     );
+  }
+
+  private calculateQty(obj: PrescribableDrug) {
+    if (obj.selectedFrequency > -1) {
+      this.getNoOfDosesForPrescription(obj);
+    }
+  }
+
+  private getNoOfDosesForPrescription(obj: PrescribableDrug) {
+    var freq = obj.doseFrequency[obj.selectedFrequency - 1];
+    obj.neededQty = 1;
+
+    // Should not be calculated  for local application etc.
+    if (obj.meal !=='ORAL')
+    {
+      return;
+    }
+    switch (obj.selectedDuration) {
+      case "Days": {
+        obj.neededQty = freq.noofDoses * obj.doseAmount * obj.doseDuration;
+        break;
+      }
+      case "Weeks": {
+        obj.neededQty = freq.noofDoses * 7 * obj.doseAmount * obj.doseDuration;
+        break;
+      }
+      case "Months": {
+        obj.neededQty = freq.noofDoses * 30 * obj.doseAmount * obj.doseDuration;
+        break;
+      }
+    }
+  }
 }
 
-export class Prescription
-{
-  symptoms:string;
-  diagnosis:string;
-  notes:string;
-  prescriptionDetailDTOS:PrescriptionDetail[];
-  patientId:number;
-  medicalServices:any[];
-  externalNote:string;
+export class Prescription {
+  symptoms: string;
+  diagnosis: string;
+  notes: string;
+  prescriptionDetailDTOS: PrescriptionDetail[];
+  patientId: number;
+  medicalServices: any[];
+  externalNote: string;
 
 }
-export class PrescriptionDetail
-{
-  drugPackageId:number;
-  drug:Drug;
-  drugId:string;
-  strength:Strength;
-  frequency:Dosefrequency;
-  amount:number;
-  duration:number;
-  intervalUnit:string;
-  meal:string;
-  doseFrequencyId:number;
+
+export class PrescriptionDetail {
+  drugPackageId: number;
+  drug: Drug;
+  drugId: string;
+  strength: Strength;
+  frequency: Dosefrequency;
+  amount: number;
+  duration: number;
+  intervalUnit: string;
+  meal: string;
+  doseFrequencyId: number;
 
 
   constructor() {
@@ -135,11 +207,10 @@ export class PrescriptionDetail
     this.strength = new Strength();
   }
 
-  public isValid():boolean
-  {
+  public isValid(): boolean {
     var passed = true;
-    passed = (this.amount > 0) && (this.duration >0) && (this.intervalUnit != null) &&
-        this.doseFrequencyId != null && this.drugPackageId != null && this.meal != null;
+    passed = (this.amount > 0) && (this.duration > 0) && (this.intervalUnit != null) &&
+      this.doseFrequencyId != null && this.drugPackageId != null && this.meal != null;
     return passed;
   }
 
